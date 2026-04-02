b80989659794d9ce9ddb212819f0c02a9ca903dd
fyfb925-gif <fyfb925@gmail.com>
2026-04-03 00:34:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def get_conn():
     return st.connection("gsheets", type=GSheetsConnection)
 
 
-@st.cache_data(ttl="10m")
 def read_sheet(worksheet: str) -> pd.DataFrame:
     conn = get_conn()
     try:
